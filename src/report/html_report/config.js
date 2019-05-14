fac6c3b7ae3c1dd48e4ce356d15b8302e361676c
Oleg Semko <semoasem@gmail.com>
2019-05-14 18:59:12 +0300

    deploy static content

---
+++ src/report/html_report/config.js
@@ -0,0 +1,30 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/test_test_0_body_0_tablet_h.png",
+        "test": "../bitmaps_test/20190514-184706/test_test_0_body_0_tablet_h.png",
+        "selector": "body",
+        "fileName": "test_test_0_body_0_tablet_h.png",
+        "label": "test",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/colored-blocks/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    }
+  ],
+  "id": "test"
+});
