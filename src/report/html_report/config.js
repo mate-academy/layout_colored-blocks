5b5a5f9ea6fd54d95db5156ddfe1bdd95d645ae7
Konstantin <overman231@gmail.com>
2019-05-15 17:50:38 +0300

    deploy static content

---
+++ src/report/html_report/config.js
@@ -0,0 +1,24 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\test_test_0_body_0_tablet_h.png",
+        "test": "..\\bitmaps_test\\20190515-174818\\test_test_0_body_0_tablet_h.png",
+        "selector": "body",
+        "fileName": "test_test_0_body_0_tablet_h.png",
+        "label": "test",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/colored-blocks/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "engineErrorMsg": "net::ERR_INVALID_REDIRECT at http://localhost:8080",
+        "error": "Test file not found C:\\Users\\Konstantin\\Desktop\\Mate Academy\\layout_colored-blocks\\backstop_data\\bitmaps_test\\20190515-174818\\test_test_0_body_0_tablet_h.png"
+      },
+      "status": "fail"
+    }
+  ],
+  "id": "test"
+});
