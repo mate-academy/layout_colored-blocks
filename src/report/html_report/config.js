fdab5945f3b399bd651c81aa8b807d5a647d6a00
Simon <semen.zelenskiy@gmail.com>
2019-08-28 11:10:36 +0300

    deploy static content

---
+++ src/report/html_report/config.js
@@ -0,0 +1,30 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/01028839250_Elementary_test_0_body_0_tablet_h.png",
+        "test": "../bitmaps_test/20190828-110925/01028839250_Elementary_test_0_body_0_tablet_h.png",
+        "selector": "body",
+        "fileName": "01028839250_Elementary_test_0_body_0_tablet_h.png",
+        "label": "Elementary test",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/colored-blocks/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.61",
+          "analysisTime": 45
+        }
+      },
+      "status": "pass"
+    }
+  ]
+});
