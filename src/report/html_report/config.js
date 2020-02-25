edb4c96f0b95029bf4378011e8ee91aa1a88bdea
Andrii Badekha <badekha@gmail.com>
2020-02-25 17:42:30 +0200

    deploy static content

---
+++ src/report/html_report/config.js
@@ -0,0 +1,23 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Entire_document.png",
+        "test": "..\\bitmaps_test\\20200225-172311\\Entire_document.png",
+        "selector": "",
+        "fileName": "Entire_document.png",
+        "label": "Entire document",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/colored-blocks/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "engineErrorMsg": "net::ERR_CONNECTION_REFUSED at http://localhost:8080/index.html",
+        "error": "Test file not found C:\\projects\\layout_colored-blocks\\backstop_data\\bitmaps_test\\20200225-172311\\Entire_document.png"
+      },
+      "status": "fail"
+    }
+  ]
+});
