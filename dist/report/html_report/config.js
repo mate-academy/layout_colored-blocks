f7f4eb51c3f1c789fcdc574cc8b7c0f98f467dc0
Dennis Priadka <dennis.priadka@gmail.com>
2020-06-30 12:53:24 +0300

    make build

--- dist/report/html_report/config.js
+++ dist/report/html_report/config.js
@@ -4,7 +4,7 @@ report({
     {
       "pair": {
         "reference": "..\\bitmaps_reference\\Entire_document.png",
-        "test": "..\\bitmaps_test\\20200627-131349\\Entire_document.png",
+        "test": "..\\bitmaps_test\\20200630-125155\\Entire_document.png",
         "selector": "document",
         "fileName": "Entire_document.png",
         "label": "Entire document",
