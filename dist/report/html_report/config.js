e9841ba9839fb4f17b6934c9201357cc6031dd25
Vasyl_Zarichnyi <vasylzarichnyi@gmail.com>
2020-06-30 14:45:12 +0300

    make build

--- dist/report/html_report/config.js
+++ dist/report/html_report/config.js
@@ -4,7 +4,7 @@ report({
     {
       "pair": {
         "reference": "..\\bitmaps_reference\\Entire_document.png",
-        "test": "..\\bitmaps_test\\20200630-142443\\Entire_document.png",
+        "test": "..\\bitmaps_test\\20200630-144235\\Entire_document.png",
         "selector": "document",
         "fileName": "Entire_document.png",
         "label": "Entire document",
